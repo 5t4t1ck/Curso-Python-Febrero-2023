ab42d4c4afb3636c948389abd19d8c5c3cfdc343
Diego Saavedra <dsaavedra88@gmail.com>
2023-02-07 21:04:18 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+_nombre = "Diego Saavedra"
+email = "dsaavedra88@gmail.com" 
+nombre_completo = "Diego Saavedra"
+nombreCompleto = "Diego Saaveddra"
+
+inicio = "Bienvenidos"
+final = "al Curso de Python"
+
+print(inicio + final)
+print(inicio + " " + final)
+print(inicio, final)

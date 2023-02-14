a34f9101610e7027f410297d69f73a0ab10353c5
Diego Saavedra <dsaavedra88@gmail.com>
2023-02-13 21:36:05 -0500

    Ej de POO

--- POO/Ejercicio_01.py
+++ POO/Ejercicio_01.py
@@ -14,3 +14,22 @@ constructora
 
 Crear 2 instancias de esta clase.
 """
+
+class Persona:
+
+    def __init__(self, Nombre, Apellido, Cedula, Edad, Correo):
+        self.Nombre = Nombre
+        self.Apellido = Apellido
+        self.Cedula = Cedula
+        self.Edad = Edad
+        self.Correo = Correo
+
+    def saludar(self):
+        return f" Hola mi nombre es {self.Nombre} {self.Apellido} mi número de cédula es {self.Cedula}, mi edad es {self.Edad} y mi correo electrónico es {self.Correo}"
+    
+
+Pedro = Persona("Pedro", "Jaramillo", 1154685245, 28, "pedro.jaramillo@gmail.com")
+Juan = Persona("Juan", "Gonzaga", 1104578963, 38, "juan.gonzaga@gmail.com")
+
+print(Pedro.saludar())
+print(Juan.saludar())

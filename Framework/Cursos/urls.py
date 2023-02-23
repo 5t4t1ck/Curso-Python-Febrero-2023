27e2252b57b5e771a5dd92eedba448484acaf7e5
Diego Saavedra <dsaavedra88@gmail.com>
2023-02-22 20:53:34 -0500

    Hola Mundo desde Django

---
+++ Framework/Cursos/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from primeraApp import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('primeraApp.urls')),
+]
